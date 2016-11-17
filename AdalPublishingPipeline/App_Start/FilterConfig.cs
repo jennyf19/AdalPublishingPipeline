4263986920d4b6561248074bf0aaede69594b010
jennyf19 <ferriesj@gmail.com>
2016-11-17 15:18:52 -0800

    Add project files.

---
+++ AdalPublishingPipeline/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AdalPublishingPipeline
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
